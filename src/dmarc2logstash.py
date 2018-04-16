d6cfc242216e07d1b625275800a534b0cb64add2
Jason Ertel <jertel@codesim.com>
2018-04-16 13:59:45 -0400

    added support for zipped files; capturing submitter domain; added more robust handling of dynamic xml elements

--- src/dmarc2logstash.py
+++ src/dmarc2logstash.py
@@ -1,11 +1,14 @@
 import argparse
 from datetime import date, datetime
+import fnmatch
 import gzip
+import zipfile
 import io
 import json
 import os
 import time
 import logging
+import re
 import signal
 import sys
 import poplib
@@ -57,48 +60,86 @@ def parseAttachments(jsonOutputFile, msg):
   for part in msg.walk():
     name = part.get_filename()
     if name is not None:
+      xmls = []
       data = part.get_payload(decode=True)
       if part.get_content_type() == "application/gzip" or name.endswith(".gzip") or name.endswith(".gz"):
         log.info("Decompressing gzip data")
-        data = gzip.GzipFile(fileobj=io.BytesIO(data)).read()
-      if parse(jsonOutputFile, str(data)):
-        success = success + 1
+        xmls.append(gzip.GzipFile(fileobj=io.BytesIO(data)).read())
+      elif part.get_content_type() == "application/zip" or name.endswith(".zip"):
+        log.info("Decompressing zip data")
+        zfile = zipfile.ZipFile(io.BytesIO(data))
+        for name in zfile.namelist():
+          if fnmatch.fnmatch(name, '*.xml'):
+            xmls.append(zfile.read(name))
+      else:
+        xmls.append(data)
+      for xml in xmls:
+        if parse(jsonOutputFile, str(xml), msg.get('subject')):
+          success = success + 1
   return success
 
-def parse(jsonOutputFile, xml):
+def parse(jsonOutputFile, xml, subject):
   if xml.find("report_metadata") > 0:
     root = ET.fromstring(xml)
     records = []
     report = {}
-    report['org_name'] = root.find('report_metadata').find('org_name').text
-    report['org_email'] = root.find('report_metadata').find('email').text
-    report['id'] = root.find('report_metadata').find('report_id').text
-    report['date_start'] = datetime.fromtimestamp(int(root.find('report_metadata').find('date_range').find('begin').text))
-    report['date_end'] = datetime.fromtimestamp(int(root.find('report_metadata').find('date_range').find('end').text))
-    report['policy_domain'] = root.find('policy_published').find('domain').text
-    report['policy_dkim'] = root.find('policy_published').find('adkim').text
-    report['policy_spf'] = root.find('policy_published').find('aspf').text
-    report['policy_p'] = root.find('policy_published').find('p').text
-    report['policy_pct'] = float(root.find('policy_published').find('pct').text)
-    for child in root.iter("record"):
-      record = dict(report)
-      record['source_ip'] = child.find('row').find('source_ip').text
-      record['count'] = int(child.find('row').find('count').text)
-      record['policy_disposition'] = child.find('row').find('policy_evaluated').find('disposition').text
-      record['policy_dkim'] = child.find('row').find('policy_evaluated').find('dkim').text
-      record['policy_spf'] = child.find('row').find('policy_evaluated').find('spf').text
-      record['identifier_header_from'] = child.find('identifiers').find('header_from').text
-      record['auth_dkim_domain'] = child.find('auth_results').find('dkim').find('domain').text
-      record['auth_dkim_result'] = child.find('auth_results').find('dkim').find('result').text
-      record['auth_spf_domain'] = child.find('auth_results').find('spf').find('domain').text
-      record['auth_spf_result'] = child.find('auth_results').find('spf').find('result').text
-      records.append(record)
-    log.info("Writing JSON records to log file")
-    with open(jsonOutputFile, 'a') as dmarcLog:
-      for record in records:
-        output = json.dumps(record, default=json_serial)
-        dmarcLog.write(output + "\n")
-    return True
+    match = re.search('.*?Submitter:\\s?([^\\s]*)\\s?', subject)
+    if match is not None and match.groups > 0:
+      report['submitter'] = match.group(1)
+    else:
+      report['submitter'] = "unknown"
+    metaData = root.find('report_metadata')
+    if metaData is not None:
+      report['org_name'] = metaData.find('org_name').text
+      report['org_email'] = metaData.find('email').text
+      report['id'] = metaData.find('report_id').text
+      report['date_start'] = datetime.fromtimestamp(int(metaData.find('date_range').find('begin').text))
+      report['date_end'] = datetime.fromtimestamp(int(metaData.find('date_range').find('end').text))
+      policyPub = root.find('policy_published')
+      if policyPub is not None:
+        report['policy_domain'] = policyPub.find('domain').text
+        report['policy_dkim'] = policyPub.find('adkim').text
+        report['policy_spf'] = policyPub.find('aspf').text
+        report['policy_p'] = policyPub.find('p').text
+        report['policy_pct'] = float(policyPub.find('pct').text)
+      for child in root.iter("record"):
+        record = dict(report)
+        row = child.find('row')
+        if row is not None:
+          record['source_ip'] = row.find('source_ip').text
+          record['count'] = int(row.find('count').text)
+          policy = row.find('policy_evaluated')
+          if policy is not None:
+            record['policy_disposition'] = policy.find('disposition').text
+            dkim = policy.find('dkim')
+            if dkim is not None:
+              record['policy_dkim'] = dkim.text
+            spf = policy.find('spf')
+            if spf is not None:
+              record['policy_spf'] = spf.text
+        identifiers = child.find('identifiers')
+        if identifiers is not None:
+          record['identifier_header_from'] = identifiers.find('header_from').text
+        authResults = child.find('auth_results')
+        if authResults is not None:
+          dkim = authResults.find('dkim')
+          if dkim is not None:
+            record['auth_dkim_domain'] = dkim.find('domain').text
+            record['auth_dkim_result'] = dkim.find('result').text
+          spf = authResults.find('spf')
+          if spf is not None:
+            record['auth_spf_domain'] = spf.find('domain').text
+            record['auth_spf_result'] = spf.find('result').text
+        records.append(record)
+
+      log.info("Writing JSON records to log file")
+      with open(jsonOutputFile, 'a') as dmarcLog:
+        for record in records:
+          output = json.dumps(record, default=json_serial)
+          dmarcLog.write(output + "\n")
+      return True
+    else:
+      log.warn("Invalid feedback; missing report_metadata element")
   else:
     log.warn("Skipping attachment that does not appear to conform to a DMARC aggregate report")
   return False
@@ -144,7 +185,7 @@ def main():
   if not server or not username or not password:
     log.error("POP3_SERVER, POP3_USERNAME, POP3_PASSWORD, and SLEEP_SECONDS are required environment variables")
   else:
-    start(server, username, password, sleepSec, jsonOutputFile)
+    start(server, username, password, int(sleepSec), jsonOutputFile)
 
 if __name__ == '__main__':
   sys.exit(main())
